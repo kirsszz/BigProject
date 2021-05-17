12da6c6c1a8b7fb8967670783668ce0b70e2461a
Kirsikka <kirsikka.tuisk@gmail.com>
2021-05-17 22:36:23 +0300

    authorization pooleli

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -17,6 +17,15 @@ const validateCampground = (req, res, next) => {
     }
 }
 
+const isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have a permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+};
 
 router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -48,7 +57,7 @@ router.get('/:id', catchAsync(async (req, res) => {
 
 }));
 
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     if (!campground) {
         req.flash('error', 'No campgrounds found!')
@@ -59,7 +68,7 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
 
 router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success', 'Successfully updated campground');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
